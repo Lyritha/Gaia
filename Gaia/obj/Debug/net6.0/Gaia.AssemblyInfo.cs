2e9eed7ad403ae63c04a1755fc489855c39fe4b0
Lyritha <Collin.rarekip@gmail.com>
2024-12-04 10:46:42 +0100

    update documents

--- Gaia/obj/Debug/net6.0/Gaia.AssemblyInfo.cs
+++ Gaia/obj/Debug/net6.0/Gaia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gaia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef080f0ac5aaad6109b4314520d159d21a6f103")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gaia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gaia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
